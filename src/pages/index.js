53e4bb55143ad01fe7eb78e0954d847fb7085fed
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-11-04 18:49:34 +0000

    score state and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
+import { useState } from "react";
+
+import styles from "../styles/Scoreboard.module.scss";
+
 export default function Home() {
+  const [homeScore, setHomeScore] = useState(11);
+  const [awayScore, setAwayScore] = useState(25);
   return (
     <>
       <h1>Scoreboard</h1>
-      <div className="scoreboard">
-        <div className="scores">
-          <div className="score">
-            <h1>HOME</h1>
-            <span>0</span>
+      <div className={styles.scoreboard}>
+        <h2 className={styles.scoreTitle}>Score</h2>
+        <div className={styles.scores}>
+          <div className={styles.score}>
+            <h3>HOME</h3>
+            <span>{homeScore}</span>
           </div>
-          <div className="score">
-            <h1>AWAY</h1>
-            <span>0</span>
+          <div className={styles.score}>
+            <h3>AWAY</h3>
+            <span>{awayScore}</span>
           </div>
         </div>
       </div>
