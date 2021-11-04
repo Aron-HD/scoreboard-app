3936dadf7dfe16ce0402f6c2f1ffc7edf1de93d2
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-11-04 18:08:34 +0000

    add sql query to get score

--- src/pages/api/scores/[scoreName].js
+++ src/pages/api/scores/[scoreName].js
@@ -1,17 +1,32 @@
+import { sql } from "@leafac/sqlite";
+import database from "../../../../database/run-db.js";
+
 export default (req, res) => {
   const httpMethod = req.method;
-
-  const placeholder = { name: "home", score: 0 };
+  const { scoreName } = req.query;
 
   switch (httpMethod) {
     case "GET":
-      res.status(200).json(placeholder);
+      const score = database.get(
+        sql`SELECT * FROM "scores" WHERE "name" = ${scoreName}`
+      );
+      console.log(scoreName);
+      console.log(score);
+      if (score) {
+        res.status(200).json(score);
+      } else {
+        res
+          .status(404)
+          .json({ message: `endpoint (${scoreName}) score not found` });
+      }
       break;
     case "PATCH":
       const payload = { name: "home", score: 1 };
       res.status(200).json(payload);
-    default:
       break;
+    default:
+      res.setHeader("Allow", ["GET", "PATCH"]);
+      res.status(405).end(`${httpMethod} method not allowed.`);
   }
   res.status(200);
 };
